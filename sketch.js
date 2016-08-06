9440f060ec3eb70779129cd61cfb015be3896252
pvespino <paula@nobilitylab.org>
2016-08-06 15:04:18 -0300

    upload initial state

---
+++ sketch.js
@@ -0,0 +1,33 @@
+//Creating animations
+
+//animations like p5 images should be stored in variables
+//in order to be displayed during the draw cycle
+var ghost, asterisk, cloud;
+
+//it's advisable (but not necessary) to load the images in the preload function
+//of your sketch otherwise they may appear with a little delay
+function preload() {
+
+//create an animation from a sequence of numbered images
+//pass the first and the last file name and it will try to find the ones in between
+ghost = loadAnimation("assets/ghost_standing0001.png", "assets/ghost_standing0007.png");
+
+cloud = loadAnimation("assets/cloud_breathing0001.png", "assets/cloud_breathing0002.png");
+  
+//create an animation listing all the images files
+asterisk = loadAnimation("assets/asterisk.png", "assets/triangle.png", "assets/square.png", "assets/cloud.png", "assets/star.png", "assets/mess.png", "assets/monster.png");
+}
+
+function setup() {
+  createCanvas(800,300);
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  //specify the animation instance and its x,y position
+  //animation() will update the animation frame as well
+  animation(ghost, 300, 150);
+  animation(asterisk, 500, 150);
+  animation (cloud, 700, 150)
+}
